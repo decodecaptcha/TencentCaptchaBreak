e6f3b73693c47607bdfd22c1e15eb8b7a7248b8a
aiden2048 <aiden2048@qq.com>
2023-08-04 17:26:24 +0800

    add

--- examples/__init__.py
+++ examples/__init__.py
@@ -4,3 +4,31 @@
 # 有需求请在邮箱联系
 
 # aidenvannet@gmail.com
+
+
+
+# 使用此类验证码的网站
+# 基本通用 :
+
+# https://007.qq.com/
+
+# https://www.sf-express.com/
+
+# http://tms.trackmeeasy.com/
+
+# https://www.qidian.com/
+
+# https://qzone.qq.com/
+
+# Web/APP/小程序都通用
+
+# ......
+
+# api 例子地址
+# QQ空间登录
+# https://github.com/aiden2048/QzoneLoginSpider
+
+
+# 免责声明
+# 本项目仅供个人学习与交流使用，严禁用于商业以及不良用途！！若读者将其用于非法用途，其造成的一切后果将自行承担且与本人无关.
+# 如果有侵犯到您的权益，请第一时间联系个人邮箱处理.
