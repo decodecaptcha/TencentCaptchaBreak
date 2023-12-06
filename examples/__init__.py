73a93f46da988dac53827fa37510ae5eb04ace80
aiden2048 <aiden2048@qq.com>
2023-12-07 05:14:56 +0800

    add

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,5 +1,5 @@
 # TencentCaptchaBreak
-# 关于某讯滑块/某讯云验证码/天御验证码/防水墙验证码 collect, pow_answer, vData, ekl 等加密参数的研究 纯协议 纯算法还原 手撕jsvmp
+# 关于腾讯滑块/腾讯云验证码/天御验证码/防水墙验证码 collect, pow_answer, vData, ekl 等加密参数的研究 纯协议 纯算法还原 手撕jsvmp
 
 # 有需求请在邮箱联系
 
